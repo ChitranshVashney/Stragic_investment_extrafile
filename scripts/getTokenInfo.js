7e562bf8ceb8c7af213e99b1ae41ca19b495761a
Chitranshu <varshneychinu1204@gmail.com>
2023-11-27 02:29:32 +0530

    m

--- scripts/getTokenInfo.js
+++ scripts/getTokenInfo.js
@@ -49,32 +49,37 @@ const getTokenInfo = async () => {
     // console.log(price.quotes[0].usdPrice);
     if (
       riskInfo[i].Return_Category === "High Return" &&
-      riskInfo[i].Risk_Category === "High Risk"
+      riskInfo[i].Risk_Category === "High Risk" &&
+      riskInfo[i].thumbnail !== ""
     ) {
       highReturnHR.push(riskInfo[i]);
     }
     if (
       riskInfo[i].Return_Category === "High Return" &&
-      riskInfo[i].Risk_Category === "Low Risk"
+      riskInfo[i].Risk_Category === "Low Risk" &&
+      riskInfo[i].thumbnail !== ""
     ) {
       highReturnLR.push(riskInfo[i]);
     }
     if (
       riskInfo[i].Return_Category === "High Return" &&
-      riskInfo[i].Risk_Category === "Medium Risk"
+      riskInfo[i].Risk_Category === "Medium Risk" &&
+      riskInfo[i].thumbnail !== ""
     ) {
       highReturnMR.push(riskInfo[i]);
     }
 
     if (
       riskInfo[i].Return_Category === "Low Return" &&
-      riskInfo[i].Risk_Category === "Low Risk"
+      riskInfo[i].Risk_Category === "Low Risk" &&
+      riskInfo[i].thumbnail !== ""
     ) {
       lowReturnLR.push(riskInfo[i]);
     }
     if (
       riskInfo[i].Return_Category === "Low Return" &&
-      riskInfo[i].Risk_Category === "Medium Risk"
+      riskInfo[i].Risk_Category === "Medium Risk" &&
+      riskInfo[i].thumbnail !== ""
     ) {
       lowReturnMR.push(riskInfo[i]);
     }
@@ -89,28 +94,35 @@ const getTokenInfo = async () => {
   console.log(riskInfo.length);
 
   // Convert the array to a JSON string
-  // const jsonString1 = JSON.stringify(lowReturnMR, null, 2);
-  // const filePath1 = "lowReturnMR.json";
-  // fs.writeFileSync(filePath1, jsonString1);
+  const jsonString1 = JSON.stringify(lowReturnMR, null, 2);
+  const filePath1 = "lowReturnMR.json";
+  fs.writeFileSync(filePath1, jsonString1);
 
-  // const jsonString2 = JSON.stringify(lowReturnLR, null, 2);
-  // const filePath2 = "lowReturnLR.json";
-  // fs.writeFileSync(filePath2, jsonString2);
+  const jsonString2 = JSON.stringify(lowReturnLR, null, 2);
+  const filePath2 = "lowReturnLR.json";
+  fs.writeFileSync(filePath2, jsonString2);
 
-  // const jsonString3 = JSON.stringify(highReturnHR, null, 2);
-  // const filePath3 = "highReturnHR.json";
-  // fs.writeFileSync(filePath3, jsonString3);
+  const jsonString3 = JSON.stringify(highReturnHR, null, 2);
+  const filePath3 = "highReturnHR.json";
+  fs.writeFileSync(filePath3, jsonString3);
 
-  // const jsonString4 = JSON.stringify(highReturnMR, null, 2);
-  // const filePath4 = "highReturnMR.json";
-  // fs.writeFileSync(filePath4, jsonString4);
+  const jsonString4 = JSON.stringify(highReturnMR, null, 2);
+  const filePath4 = "highReturnMR.json";
+  fs.writeFileSync(filePath4, jsonString4);
 
-  // const jsonString5 = JSON.stringify(highReturnLR, null, 2);
-  // const filePath5 = "highReturnLR.json";
-  // fs.writeFileSync(filePath5, jsonString5);
-
-  const jsonString5 = JSON.stringify(riskInfo, null, 2);
-  const filePath5 = "allToken.json";
+  const jsonString5 = JSON.stringify(highReturnLR, null, 2);
+  const filePath5 = "highReturnLR.json";
   fs.writeFileSync(filePath5, jsonString5);
+
+  let allToken = [];
+  allToken.push(lowReturnMR);
+  allToken.push(lowReturnLR);
+  allToken.push(highReturnHR);
+  allToken.push(highReturnMR);
+  allToken.push(highReturnLR);
+
+  const jsonString6 = JSON.stringify(allToken, null, 2);
+  const filePath6 = "allToken.json";
+  fs.writeFileSync(filePath6, jsonString6);
 };
 getTokenInfo();
